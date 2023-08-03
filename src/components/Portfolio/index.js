63f000b20598308571ed5102d04d966f5cba8ded
Gagan Sunar <gagaan.sunaar@gmail.com>
2023-08-04 00:48:03 +0545

    first portfoili changes

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import PortfolioImage from "../../assets/images/Portfolio.jpg";
+// import { getDocs, collection } from 'firebase/firestore';
+// import { db } from '../../firebase';
+
+const Portfolio = () => { 
+    const [letterClass, setLetterClass] = useState('text-animate');
+    // const [portfolio, setPortfolio] = useState([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    });
+
+    useEffect(() => {
+        getPortfolio();
+    }, []);
+
+    const getPortfolio = async () => {
+        // const querySnapshot = await getDocs(collection(db, 'portfolio'));
+        // setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+    }
+
+    const renderPortfolio = () => {
+        return (
+            <div className="images-container">
+                            <div className="image-box">
+                                <img 
+                                src={PortfolioImage}
+                                className="portfolio-image"
+                                alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">Port Name</p>
+                                    <h4 className="description">description</h4>
+                                    <button
+                                        className="btn"
+                                    >View</button>
+                                </div>
+                            </div>
+            </div>
+        );
+    }
+
+
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+                </h1>
+                <div>{renderPortfolio()}</div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
+}
+
+export default Portfolio;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,123 @@
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-g.png'
+import LogoSubtitle from '../../assets/images/logog_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/gagan-sunar-01375322a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Gagan320"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCP9VMiad4L6pS9HkWQksnmA"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://join.skype.com/invite/tnEy1xLFr3WA" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+     
+    </div>
+  )
+}
+
+export default Sidebar
